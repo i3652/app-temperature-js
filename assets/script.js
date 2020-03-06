3924788525e2566aa6b69b49ce744f5f141e2295
GOMES VITORINO Marvin <marvin.gomes.vito@gmail.com>
2020-03-06 17:01:42 +0100

    Mise en ligne du site

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,20 @@
-const city = "montlhéry";
-const country = "fr";
-const paragraphElement = document.querySelector("p");
-fetch(
-  `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=489d758cad27ef9f5cf4e242b9c4adbf`
-)
-  .then(response => response.json())
-  .then(data => {
-    console.dir(data);
-    console.log(data.main.temp);
-    let temp = Math.round(data.main.temp - 273, 15);
-    paragraphElement.textContent =
-      temp +
-      `°C
+function affiche(event) {
+  event.preventDefault();
+  const city = document.getElementById("inputVilleDep").value;
+  const country = document.getElementById("inputPays").value;
+  const paragraphElement = document.querySelector("p");
+  console.log(city);
+  console.log(country);
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=489d758cad27ef9f5cf4e242b9c4adbf`
+  )
+    .then(response => response.json())
+    .then(data => {
+      let temp = Math.round(data.main.temp - 273, 15);
+      paragraphElement.textContent = `${temp} °C
        à 
        ${data.name}`;
-  });
+    });
+}
+formulaire = document.querySelector(".ui.form");
+formulaire.addEventListener("submit", affiche);
